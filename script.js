c441dea70954da9cb53f04773e984d177f5edc2e
Ulimativo <ulrich.raab@outlook.com>
2022-12-29 09:06:01 +0100

    script working properly

--- script.js
+++ script.js
@@ -2,9 +2,28 @@
 // 3 sets of data = parts of the sentence.
 
 const messageComponents = {
-  helpverb: ["should", "need to", "must"],
-  verb: ["work", "learn", "relax"],
-  end: ["more often", "always", "never"],
+  helpverb: ["should", "need to", "must", "have to"],
+  verb: ["work", "learn", "relax", "sing", "dance"],
+  end: ["more often", "always", "never", "daily", "weekly", "once a year"],
 };
 
-console.log(messageComponents.helpverb);
+// var item = items[Math.floor(Math.random()*items.length)];
+
+function randomizer(items) {
+  let item = items[Math.floor(Math.random() * items.length)];
+  return item;
+}
+
+function message() {
+  message =
+    "You " +
+    randomizer(messageComponents.helpverb) +
+    " " +
+    randomizer(messageComponents.verb) +
+    " " +
+    randomizer(messageComponents.end) +
+    ".";
+  return message;
+}
+
+console.log(message());
