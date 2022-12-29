c0c6a79bf226c5affb78aa78c26cff75d3ae6a20
Ulimativo <ulrich.raab@outlook.com>
2022-12-29 08:59:51 +0100

    added messageComponents object

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello!");
+// Topic?
+// 3 sets of data = parts of the sentence.
+
+const messageComponents = {
+  helpverb: ["should", "need to", "must"],
+  verb: ["work", "learn", "relax"],
+  end: ["more often", "always", "never"],
+};
+
+console.log(messageComponents.helpverb);
